c317c7f610028a990365b40887d79ded2b909bac
PawelekKlocek <116948265+PawelekKlocek@users.noreply.github.com>
2023-12-27 13:44:10 +0100

    Log4j configuration

--- src/project/Main.java
+++ src/project/Main.java
@@ -4,7 +4,13 @@ import org.apache.logging.log4j.*;
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
+        System.out.println("programik");
         logger.info("info");
         logger.error("error");
+        logger.fatal("fatal");
+        logger.warn("warn");
+        logger.debug("debug");
+
+
     }
 }
