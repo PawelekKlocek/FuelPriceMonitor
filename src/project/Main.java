cbfd47098452948bd918c637e7c3593dfde7c7d9
PawelekKlocek <116948265+PawelekKlocek@users.noreply.github.com>
2023-12-27 12:26:11 +0100

    Log4j application

--- src/project/Main.java
+++ src/project/Main.java
@@ -4,7 +4,6 @@ import org.apache.logging.log4j.*;
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
-        System.out.println("this is sout");
         logger.info("info");
         logger.error("error");
     }
